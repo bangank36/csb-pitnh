307d33a5137c6a6947c040629f4387893554ed1c
Code Sandbox <hello@codesandbox.io>
2021-08-04 07:56:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dbfcbf3df5573e61d81cdb6db30a76b7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573ec172a9a71dc2cc4bfff19ab3e79c",
+    "url": "https://bangank36.github.io/csb-pitnh/index.html"
+  },
+  {
+    "revision": "4731436455b4981fe832",
+    "url": "https://bangank36.github.io/csb-pitnh/static/js/2.17c2cc69.chunk.js"
+  },
+  {
+    "revision": "b25beb46bc48d563a885",
+    "url": "https://bangank36.github.io/csb-pitnh/static/js/main.1e76e9e6.chunk.js"
+  },
+  {
+    "revision": "aacd232ffbe064e67f57",
+    "url": "https://bangank36.github.io/csb-pitnh/static/js/runtime~main.57819557.js"
+  }
+]);
